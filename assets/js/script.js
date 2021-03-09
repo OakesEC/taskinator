71f64308f1d9c940b44a20d790e5caa678e0a707
Liz Oakes <liz77oakes@gmail.com>
2021-03-09 16:16:50 -0500

    ask bcs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var taskToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     taskToDoE1.appendChild(listItemEl);
-}
+};
 
-buttonE1.addEventListener("click", createTaskHandler);
-
+formEl.addEventListener("submit", createTaskHandler);
 
 
 
